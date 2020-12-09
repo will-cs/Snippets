e45a554c3bae6c6852a50041948b68f3e0c6e2b7
William <willgithub@outlook.com>
2020-12-09 13:11:20 -0500

    Create InsertionSort.java

---
+++ Java/InsertionSort.java
@@ -0,0 +1,12 @@
+private void insertionSort(int[] Arr, boolean ascending) {
+	int temp, j;
+    for (int i = 1; i < Arr.length; i++) {
+    	temp = Arr[i];
+        j = i - 1;
+        while (j >= 0 && ((Arr[j] > temp && ascending) || (Arr[j] < temp && !ascending))) {
+        	Arr[j + 1] = Arr[j];
+            j--;
+		}
+		Arr[j + 1] = temp;
+	}
+}
