88403793a90821e8bd687a6d4829cad06e375abd
William <willgithub@outlook.com>
2020-12-09 13:09:53 -0500

    Update SelectionSort.java

--- Java/SelectionSort.java
+++ Java/SelectionSort.java
@@ -1,4 +1,4 @@
-private int[] selectionSort(int[] Arr, boolean ascending) {
+private void selectionSort(int[] Arr, boolean ascending) {
 	int temp;
 	for (int i = 0; i < Arr.length - 1; i++) {
     	for (int j = i + 1; j < Arr.length; j++) {
@@ -9,5 +9,4 @@ private int[] selectionSort(int[] Arr, boolean ascending) {
 			}
 		}
 	}
-    return Arr;
 }
