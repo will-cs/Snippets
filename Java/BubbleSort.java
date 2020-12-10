bef6584f5df85185c4ac8e3e4e151de5ada0a098
William <willgithub@outlook.com>
2020-12-10 10:05:13 -0500

    Update BubbleSort.java
    Indentation

--- Java/BubbleSort.java
+++ Java/BubbleSort.java
@@ -1,17 +1,17 @@
 private void bubbleSort(int[] Arr, boolean ascending) {
-    boolean swap = true;
-    int bottom = Arr.length - 1;
-    int temp;
+	boolean swap = true;
+	int bottom = Arr.length - 1;
+	int temp;
 	while (swap) {
-        swap = false;
+		swap = false;
 		for (int j = 0; j < bottom; j++) {
 			if ((Arr[j] > Arr[j + 1] && ascending) || (Arr[j] < Arr[j + 1] && !ascending)) {
-                swap = true;
-                temp = Arr[j];
-                Arr[j] = Arr[j + 1];
-            	Arr[j + 1] = temp;
-        	}
-        }
-    	bottom--;
+				swap = true;
+				temp = Arr[j];
+				Arr[j] = Arr[j + 1];
+				Arr[j + 1] = temp;
+			}
+		}
+		bottom--;
 	}
 }
