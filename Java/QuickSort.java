7dda5bdec0f90c1fe169b2ddc4706f7885a3ad3e
William <willgithub@outlook.com>
2020-12-10 09:59:17 -0500

    Update QuickSort.java
    Indentation

--- Java/QuickSort.java
+++ Java/QuickSort.java
@@ -22,8 +22,8 @@ private int pivot(int[] Arr, int low, int high, boolean ascending) {
 
 private void quickSort(int[] Arr, int low, int high, boolean ascending) {
 	if (low < high) {
-    	int p = pivot(Arr, low, high, ascending);
-        quickSort(Arr, low, p - 1, ascending);
-        quickSort(Arr, p + 1, high, ascending);
+		int p = pivot(Arr, low, high, ascending);
+		quickSort(Arr, low, p - 1, ascending);
+		quickSort(Arr, p + 1, high, ascending);
 	}
 }
