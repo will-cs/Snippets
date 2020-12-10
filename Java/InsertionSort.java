051f16312e0d5acc8af2a44ab5fb114a3398d651
William <willgithub@outlook.com>
2020-12-10 10:07:23 -0500

    Update InsertionSort.java
    Indentation

--- Java/InsertionSort.java
+++ Java/InsertionSort.java
@@ -1,11 +1,11 @@
 private void insertionSort(int[] Arr, boolean ascending) {
 	int temp, j;
-    for (int i = 1; i < Arr.length; i++) {
-    	temp = Arr[i];
-        j = i - 1;
-        while (j >= 0 && ((Arr[j] > temp && ascending) || (Arr[j] < temp && !ascending))) {
-        	Arr[j + 1] = Arr[j];
-            j--;
+	for (int i = 1; i < Arr.length; i++) {
+		temp = Arr[i];
+		j = i - 1;
+		while (j >= 0 && ((Arr[j] > temp && ascending) || (Arr[j] < temp && !ascending))) {
+			Arr[j + 1] = Arr[j];
+			j--;
 		}
 		Arr[j + 1] = temp;
 	}
