c1aefad5b6a3418aa227949730da17432ea3721a
William <willgithub@outlook.com>
2020-12-10 10:06:04 -0500

    Update SelectionSort.java
    Indentation

--- Java/SelectionSort.java
+++ Java/SelectionSort.java
@@ -1,11 +1,11 @@
 private void selectionSort(int[] Arr, boolean ascending) {
 	int temp;
 	for (int i = 0; i < Arr.length - 1; i++) {
-    	for (int j = i + 1; j < Arr.length; j++) {
-        	if ((Arr[i] > Arr[j] && ascending) || (Arr[i] < Arr[j] && !ascending)) {
-            	temp = Arr[i];
-                Arr[i] = Arr[j];
-                Arr[j] = temp;
+		for (int j = i + 1; j < Arr.length; j++) {
+			if ((Arr[i] > Arr[j] && ascending) || (Arr[i] < Arr[j] && !ascending)) {
+				temp = Arr[i];
+				Arr[i] = Arr[j];
+				Arr[j] = temp;
 			}
 		}
 	}
