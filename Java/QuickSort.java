ae643d08b9a73307820cb535971d950191f51977
William <willgithub@outlook.com>
2021-01-08 11:54:34 -0500

    Update QuickSort.java

--- Java/QuickSort.java
+++ Java/QuickSort.java
@@ -1,29 +1,28 @@
-private int pivot(int[] Arr, int low, int high, boolean ascending) {
-	int pivotValue = Arr[high];
-	int pivotPoint = (low - 1);
-	int temp;
+private void quickSortHelper(int[] Arr, int low, int high, boolean ascending) {
+	if (low < high) {
+		int pivotValue = Arr[high];
+		int pivotPoint = (low - 1);
+		int temp;
 
-	for (int i = low; i < high; i++) {
-		if ((Arr[i] < pivotValue && ascending) || (Arr[i] > pivotValue && !ascending)) {
-			pivotPoint++;
-			temp = Arr[pivotPoint];
-			Arr[pivotPoint] = Arr[i];
-			Arr[i] = temp;
+		for (int i = low; i < high; i++) {
+			if ((Arr[i] < pivotValue && ascending) || (Arr[i] > pivotValue && !ascending)) {
+				pivotPoint++;
+				temp = Arr[pivotPoint];
+				Arr[pivotPoint] = Arr[i];
+				Arr[i] = temp;
+			}
 		}
+
+		pivotPoint++;
+		temp = Arr[pivotPoint];
+		Arr[pivotPoint] = Arr[high];
+		Arr[high] = temp;
+
+		quickSortHelper(Arr, low, pivotPoint - 1, ascending);
+		quickSortHelper(Arr, pivotPoint + 1, high, ascending);
 	}
-
-	pivotPoint++;
-	temp = Arr[pivotPoint];
-	Arr[pivotPoint] = Arr[high];
-	Arr[high] = temp;
-
-    return pivotPoint;
 }
 
-private void quickSort(int[] Arr, int low, int high, boolean ascending) {
-	if (low < high) {
-		int p = pivot(Arr, low, high, ascending);
-		quickSort(Arr, low, p - 1, ascending);
-		quickSort(Arr, p + 1, high, ascending);
-	}
+private void quickSort(int[] Arr, boolean ascending) {
+	quickSortHelper(Arr, 0, Arr.length - 1, ascending);
 }
